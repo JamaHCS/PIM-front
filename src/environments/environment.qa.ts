3dd071dd3ca2d684b8d6d443f98d94d517cc436f
Jama <jama117@hotmail.com>
2025-01-26 04:22:04 -0600

    basic configurations

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,8 @@
+const API_URL = `https://pimapi.jamadev.com`;
+
+export const environment = {
+  app: `https://pim.jamadev.com`,
+  api: `${API_URL}/api/`,
+  version: 'v0.0.1q',
+  node_version: 'v18.20.3',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  app: ``,
+  api: ``,
+  version: '',
+  node_version: '',
+};
